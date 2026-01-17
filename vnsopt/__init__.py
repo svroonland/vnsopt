edd17872d0ac1df1c548650c0e8bd74dc802f8f9
Steven Vroonland <steven@dataworkz.nl>
2026-01-17 10:04:06 +0100

    Fix module export

--- vnsopt/__init__.py
+++ vnsopt/__init__.py
@@ -1,3 +1,5 @@
-from vnsopt.types import *
-from vnsopt.vnd import *
-from vnsopt.vns import *
+from vnsopt.types import FitnessScore, FitnessFunction, S, Neighbourhood
+from vnsopt.vns import variable_neighbourhood_search
+from vnsopt.vnd import vnd
+
+__all__ = ["FitnessScore", "FitnessFunction", "Neighbourhood", "S", "variable_neighbourhood_search", "vnd"]
