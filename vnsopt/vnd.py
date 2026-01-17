d518035da176ff0065aee8304786180183c946d6
Steven Vroonland <steven@dataworkz.nl>
2026-01-17 09:55:18 +0100

    epsilon in vnd + docs

--- vnsopt/vnd.py
+++ vnsopt/vnd.py
@@ -4,15 +4,21 @@ from vnsopt.types import Neighbourhood, FitnessFunction, FitnessScore
 
 
 def vnd[S](
-    neighbourhoods: List[Neighbourhood[S]], s: S, objective: FitnessFunction[S], verbosity: int = 0
+    neighbourhoods: List[Neighbourhood[S]],
+    s: S,
+    objective: FitnessFunction[S],
+    epsilon: float = 1e-6,
+    verbosity: int = 0,
 ) -> tuple[S, FitnessScore]:
     """
     Variable Neighbourhood Descent (VND) algorithm.
 
-    :param neighbourhoods:
-    :param s:
-    :param objective:
-    :return:
+    :param neighbourhoods: List of neighbourhoods to explore sequentially
+    :param s: Initial solution
+    :param objective: Fitness function
+    :param epsilon: Minimum improvement in fitness for a solution to be considered better
+    :param verbosity: Log verbosity level. 0 or 1 currently.
+    :return: Tuple of the improved solution and its fitness score
     """
     k = 0
     best_s = s
@@ -29,7 +35,7 @@ def vnd[S](
 
             for new_s, delta in gen:
                 print(f"[VND] k={k}: Evaluated neighbour with delta {delta}") if verbosity > 0 else None
-                if delta < -0.0000001:
+                if delta < -epsilon:
                     best_s = new_s
                     best_score = best_score + delta
                     print(f"[VND] k={k}: improved solution to {best_score}") if verbosity > 0 else None
